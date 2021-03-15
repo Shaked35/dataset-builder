426f6a073c0a7348c5564c1d0d3987feba342cb4
shaked <shakeda35@gmail.com>
2021-03-15 06:47:03 +0200

    fix abstract

--- src/main/java/com/builder/processes/AbstractProcess.java
+++ src/main/java/com/builder/processes/AbstractProcess.java
@@ -3,7 +3,6 @@ package com.builder.processes;
 import org.bson.Document;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 public interface AbstractProcess {
     void add(String header, String conditionType, String conditionValue);
